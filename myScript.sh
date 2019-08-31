a87c216e7f778bf259d1ad7706ce11644aa0b158
Shefali Emmanuel <emmanuelsn@g.cofc.edu>
2019-08-31 17:04:39 -0400

    Update myScript.sh

--- myScript.sh
+++ myScript.sh
@@ -1,14 +1,18 @@
 #! /bin/bash
 echo "Hello $USER!<br>" >> myScript.html
+echo "<br>" >> myScript.html
 echo "What's the root structure?<br>" >> myScript.html
 pwd >> myScript.html
+echo "<br>" >> myScript.html
 echo "<br>What's inside Lamp?<br>" >> myScript.html
 ls >> myScript.html
 echo "<br>" >> myScript.html
 echo "<br>What's inside TestAutomation?<br>" >> myScript.html
 cd TestAutomation
-ls >> myScript.html
+ls >> "/Users/shefaliemmanuel 1/Lamp/myScript.html"
 cd ..
+echo "<br>" >> myScript.html
+echo "<br>done!<br>" >> myScript.html
 open myScript.html
 
 exit 0
