6e8380dd5cb80c00771331a2912fe64331098916
DwayneFerguson <54327160+DwayneFerguson@users.noreply.github.com>
2019-09-09 20:11:48 -0400

    Delete myList.sh

--- myList.sh
+++
@@ -1,7 +0,0 @@
-#/bin/env bash
-
-echo "This script shows the top-level directory contents of the directory it is contained in and diesplays it in a browser."
-
-ls > dirlist.html
-firefox dirlist.html
-rm dirlist.html
