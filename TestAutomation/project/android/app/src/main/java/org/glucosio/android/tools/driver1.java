26f2bfd4336fa158a69c532d4c2d2b1e1a86e480
DwayneFerguson <54327160+DwayneFerguson@users.noreply.github.com>
2019-10-17 09:23:07 -0400

    Updated Testcase files and ./runAllTests.sh

---
+++ TestAutomation/project/android/app/src/main/java/org/glucosio/android/tools/driver1.java
@@ -0,0 +1,41 @@
+package org.glucosio.android.tools;
+//import java.io.FileNotFoundException;
+import java.lang.reflect.Method;
+//import static java.sql.Types.NULL;
+
+
+
+
+public class driver1 {
+    public static void main(String[] args) throws Exception {
+        double input1;
+        if(args.length >0 ){
+            System.out.printf("%s,%s,%s\n",args[0],args[1],lbToKgTest(Double.parseDouble(args[2])));
+        }
+    }
+
+
+    public static String lbToKgTest(double lb) throws Exception {
+        GlucosioConverter testObj = new GlucosioConverter();
+        Class testCls = testObj.getClass();
+        try {
+            Method lbToKgMethod = testCls.getDeclaredMethod("lbToKg", double.class);
+            lbToKgMethod.setAccessible(true);
+            Object lbToKgTestObj = lbToKgMethod.invoke(testObj, lb);
+//            System.out.printf("test results are: %s\n", lbToKgTestObj);
+            String results = lbToKgTestObj.toString();
+            return results;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return "Test did not take place";
+    }
+}
+
+
+
+
+
+
+
+
--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -1,49 +1,62 @@
 #!/bin/bash 
-?
+
+#/TestAutomation/project/android/app/src/main/java/org/glucosio/android/tools
+
+#navigate to folder location of driver
+cd ../project/android/app/src/main/java/org/glucosio/android/tools
+
 #go to location of text files
-Java -cp .org.glucosio.android.tools.Driver'input X input Y input Z'
-?
+java -cp .org.glucosio.android.tools.Driver'input X input Y input Z'
+
 #gradle build
-?
+
 #loop to process all test files
 for i in $(seq 1 25); do 
-?
+
     #parse text file to save variables and display oracle
     #rm command will remove current info from file before adding new
     rm sampletest$i.txt 2>/dev/null
-?
+
     FIRSTLINE=`sed -n 1p testcase$i.txt`
     echo "Test Case Number: " $FIRSTLINE$'\n' >> sampletest$i.txt
-?
-?
+
+
     SECONDLINE=`sed -n 3p testcase$i.txt`
     echo "Method Signature: " $SECONDLINE$'\n' >> sampletest$i.txt
-?
-?
+
     THIRDLINE=`sed -n 5p testcase$i.txt`
     echo "Input Value: " $THIRDLINE$'\n' >> sampletest$i.txt
-?
-?
+
+
     FOURTHLINE=`sed -n 7p testcase$i.txt`
     echo "Expected Output: " $FOURTHLINE$'\n' >> sampletest$i.txt
-   
-?
-    #compile driver
-    javac driver$i.java
-?
+    
+    #copy driver$i.java to correct project location prior to compiling
+    cp driver$i.java ../project/android/app/src/main/java/org/glucosio/android/tools
+    #compile driver & compile the .java file that contains the method we want to test this case
+    
+    PWD="$(pwd)"
+    javac -cp ${PWD}/project/android/app/src/main/java/org/glucosio/android/tools project/android/app/src/main/java/org/glucosio/android/tools/driver$i.java project/android/app/src/main/java/org/glucosio/android/tools/GlucosioConverter.java
+
+    ## this requires navigation to the appropriate directory to function. Above command will execute properly.
+    #javac driver$i.java GlucosioConverter.java
+    
+    #navigate to folder location to compile (Have not figured out how to do this from current directory)
+    cd ../project/android/app/src/main/java
+
     #pass in method name and input commands to corresponding driver
+    #sends output to the reports folder (final file name not identified)
     #SECONDLINE is method signature, THIRDLINE is input value
-    java driver$i.java SECONDLINE, THIRDLINE
-?
-    #compile the .java file that contains the method we want to test this case
-    javac GlucosioConverter.java
-?
-   
+    java -cp . org.glucosio.android.tools.driver$i SECONDLINE, THIRDLINE >> ../../../../../../reports/output.txt
+
+    #navigate back to scripts folder
+    cd ../../../../../../scripts
+       
     #display actual
-?
+
     #save to text file
-?
+
 #end for loop
 done
-?
-    #open web browser with final expected and actual outputs
+
+    #open web browser with final expected and actual output
